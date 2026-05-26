d67f215bca6ff5078f1f672571a11141a2320a5b
gkapacjkt <gkapacjkt@gmail.com>
2026-05-26 18:08:57 +0700

    Update ContactForm.tsx

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { motion, AnimatePresence } from 'motion/react';
+import { motion, AnimatePresence } from 'framer-motion';
 import { User, Mail, BookOpen, MessageSquare, Send, Trash2, TriangleAlert, CircleCheck } from 'lucide-react';
 import { ContactSubmission } from '../types';
 
